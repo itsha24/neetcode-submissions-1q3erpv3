a0a61431cfb8adf2c2ccb2aef7605f8518b56bdc
Hassan Awan <47327039+itsha24@users.noreply.github.com>
2026-04-02 15:10:11 -0400

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int left = 0;
+
+        for (int i = 0; i < prices.length; i++)
+        {
+            if (prices[left] > prices[i])
+                left = i;
+            else
+                profit = Math.max(profit, prices[i] - prices[left]);
+            
+        }
+
+        return profit;
+    }
+}
